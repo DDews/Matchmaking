5e91e8d70d64bf901e90b6e50ad00a6a7ef0c42a
Dan Dews <danielricharddews@gmail.com>
2017-11-24 23:54:22 -0700

    maybe?

--- listen.js
+++ listen.js
@@ -1,14 +1,25 @@
 "use strict";
 
+const TIMEOUT = 60 * 1000; // 60 seconds
 var http = require("http");
+var jsonFile = require("jsonfile");
+var rooms = jsonFile.readFile(file,function (err) {
+  if (err) console.log(err);
+});
 
 class Server
 {
+    cleanRooms() {
+      var time = +new Date();
+      for (var room in rooms) {
+        if (time - room.heartbeaet > TIMEOUT) delete rooms[room];
+      }
+    }
     constructor()
     {
         this.port = 8080;
         this.ip = "localhost";
-
+        cleanRooms();
         this.start();
     }
 
@@ -53,17 +64,21 @@ class Server
             });
             req.on("end", () =>
             {
+                cleanRooms();
                 // Now that we have all data from the client, we process it
                 console.log("Received data: " + body);
                 // Split the key / pair values and print them out
+                var obj = {};
                 var vars = body.split("&");
                 for (var t = 0; t < vars.length; t++)
                 {
                     var pair = vars[t].split("=");
                     var key = decodeURIComponent(pair[0]);
                     var val = decodeURIComponent(pair[1]);
+                    obj[key] = val;
                     console.log(key + ":" + val);
                 }
+
                 // Tell Unity that we received the data OK
                 res.writeHead(200, {"Content-Type": "text/plain"});
                 res.end("OK");
@@ -71,9 +86,14 @@ class Server
         }
         else
         {
+            cleanRooms();
             // Tell Unity that the HTTP method was not allowed
             res.writeHead(405, "Method Not Allowed", {"Content-Type": "text/html"});
-            res.end("Error 405");
+            var out = [];
+            for (var room in rooms) {
+              out.push(rooms[room]);
+            }
+            res.end(JSON.stringify(out));
         }
     }
 
